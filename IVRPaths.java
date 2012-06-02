89aa651f2eaf0022ac20303d6d9c6d4f4b62298b
hadasbenami <benamihadas@gmail.com>
2012-06-02 17:31:02 +0300

    fix paths according to voicexml

--- IVRPaths.java
+++ IVRPaths.java
@@ -34,41 +34,38 @@ public class IVRPaths {
 	
 	public void Init(){
 		
-		int[] path1 = {1,1,};
-		//int[] path2 = {1,1,2};
-		//int[] path3 = {1,1,3};
-		//int[] path4 = {1,1,4};
-		
-		int[] path5 = {1,2};
-		int[] path6 = {1,3};
+		int[] path0 = {1,1};
 		
 		
-		int[] path7 = {2,2};
-		int[] path8 = {2,3};
-		int[] path9 = {2,4};
+		int[] path1 = {1,2};
+		int[] path2 = {1,3};
 		
 		
-		int[] path10 = {3,9};
-		int[] path11 = {3,1};
-		int[] path12 = {3,2};
-		int[] path13 = {3,3};
-		int[] path14 = {3,4};
+		int[] path3 = {2,2};
+		int[] path4 = {2,3};
+		int[] path5 = {2,4};
 		
+		
+		int[] path6 = {3,9};
+		int[] path7 = {3,1};
+		int[] path8 = {3,2};
+		int[] path9 = {3,3};
+		
+		
+		int[] path10 = {4};
+		
+		mIVRPaths.add(path0);
 		mIVRPaths.add(path1);
-		//mIVRPaths.add(path2);
-	//	mIVRPaths.add(path3);
-	//	mIVRPaths.add(path4);
+		mIVRPaths.add(path2);
+		mIVRPaths.add(path3);
+		mIVRPaths.add(path4);
 		mIVRPaths.add(path5);
 		mIVRPaths.add(path6);
 		mIVRPaths.add(path7);
 		mIVRPaths.add(path8);
 		mIVRPaths.add(path9);
 		mIVRPaths.add(path10);
-		mIVRPaths.add(path11);
-		mIVRPaths.add(path12);
-		mIVRPaths.add(path13);
-		mIVRPaths.add(path14);
-
+		
 		
 	}
 
